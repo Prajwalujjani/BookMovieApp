321cc8f76a7dc96484e455955bbc18ba914232e6
Prajwalujjani <prajwalbunty22@gmail.com>
2021-09-01 12:13:57 +0530

    Implemented movie datails page and some minor changes into it

--- src/commom/header/Header.js
+++ src/commom/header/Header.js
@@ -1,109 +1,145 @@
-import React,{useEffect} from 'react'
-import "./Header.css"
-import ReactLogo from '../../assets/logo.svg';
-import { Button } from '@material-ui/core'
-import Modal from 'react-modal';
-import ReactDOM from 'react-dom';
-import MaterialTabs from './MaterialTabs';
-
-
-
+import React, { useEffect, useState } from "react";
+import "./Header.css";
+import ReactLogo from "../../assets/logo.svg";
+import { Button } from "@material-ui/core";
+import Modal from "react-modal";
+import ReactDOM from "react-dom";
+import MaterialTabs from "./MaterialTabs";
 
 const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)'
-    }
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
 };
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 function Header(props) {
+  const userDetails = JSON.parse(localStorage.getItem("userDetails")) ;
 
-    // let subtitle;
-    const [modalIsOpen, setIsOpen] = React.useState(false);
-    const [loginStatus, setLoginStatus] = React.useState(false);
+  const initialLoginStatus = userDetails&&userDetails.loginStatus
+  
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [loginStatus, setLoginStatus] = React.useState(initialLoginStatus);
 
-    function openModal() {
-        setIsOpen(true);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  // function afterOpenModal() {
+  //     // references are now sync'd and can be accessed.
+  //     subtitle.style.color = '#f00';
+  // }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  // function isLoggedIn(){
+  //     const login=JSON.parse(localStorage.getItem("userDetails"))
+  //     return login&&login.loginStatus
+  // }
+
+  function handleLogout() {
+    const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+    userDetails.loginStatus = false;
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    setLoginStatus(false);
+  }
+
+  
+
+  function updateLoginStatus() {
+    const login = JSON.parse(localStorage.getItem("userDetails"));
+    if (login && login.loginStatus) {
+      setLoginStatus(true);
+    } else {
+      setLoginStatus(false);
     }
+  }
 
-    // function afterOpenModal() {
-    //     // references are now sync'd and can be accessed.
-    //     subtitle.style.color = '#f00';
-    // }
-
-    function closeModal() {
-        setIsOpen(false);
+  const bookShowHandler = () => {
+   
+    if (!loginStatus) {
+      
+      setIsOpen(true);
+      return;
     }
-
-    // function isLoggedIn(){
-    //     const login=JSON.parse(localStorage.getItem("userDetails"))
-    //     return login&&login.loginStatus       
-    // }
-
-    function handleLogout(){
-        const userDetails=JSON.parse(localStorage.getItem("userDetails"))
-        userDetails.loginStatus = false
-        localStorage.setItem("userDetails", JSON.stringify(userDetails))
-        setLoginStatus(false)
-
-        
+    if (props.movieId && props.history) {
+      props.history.push("/bookshow/" + props.movieId);
     }
+  };
 
-    function updateLoginStatus(){
-        const login=JSON.parse(localStorage.getItem("userDetails"))
-        if(login&&login.loginStatus){
-            setLoginStatus(true)
-        }else{
-            setLoginStatus(false)
-        }
-    }
+  useEffect(updateLoginStatus);
 
-    useEffect(updateLoginStatus)
+  return (
+    <div>
+      <div>
+        <div className="header">
+          <img className="logo rotate linear infinite" src={ReactLogo} alt="" />
 
-    return (
-        <div>
-            <div>
-                <div {...props} className="header">
-                    <img className="logo rotate linear infinite" src={ReactLogo} alt='' />
-                    
-                    
-                    {!loginStatus&&<Button variant="contained" className="btnLogin" onClick={() => { openModal() }} >Login</Button>}
-                    {loginStatus&&<Button variant="contained" className="btnLogin" onClick={handleLogout}>Logout</Button>}
-                    <Modal
-                        isOpen={modalIsOpen}
-                        // onAfterOpen={afterOpenModal}
-                        onRequestClose={closeModal}
-                        style={customStyles}
-                        contentLabel="Example Modal"
-                        ariaHideApp={false}>
+          <div className="groupingButton">
 
-                            <MaterialTabs {...props} setLoginStatus={setLoginStatus} setIsOpen={setIsOpen}/>
-                    </Modal>
-                </div>
+          {props.movieId && (
+            <Button
+              variant="contained"
+              color="primary"
+              id="btnBookShow"
+              onClick={bookShowHandler}
+            >
+              Book Show
+            </Button>
+          )}
 
-            </div>
+          {!loginStatus && (
+            <Button
+              variant="contained"
+              className="btnLogin"
+              onClick={() => {
+                openModal();
+              }}
+            >
+              Login
+            </Button>
+          )}
+          {loginStatus && (
+            <Button
+              variant="contained"
+              className="btnLogin"
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+          )}
+
+          </div>
+         
+
+         
+          <Modal
+            isOpen={modalIsOpen}
+            // onAfterOpen={afterOpenModal}
+            onRequestClose={closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+            ariaHideApp={false}
+          >
+            <MaterialTabs
+              {...props}
+              setLoginStatus={setLoginStatus}
+              setIsOpen={setIsOpen}
+            />
+          </Modal>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-ReactDOM.render(<Header />,document.getElementById('root') );
+ReactDOM.render(<Header />, document.getElementById("root"));
 
-export default Header
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default Header;
--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Fragment } from "react";
 import Header from "../../commom/header/Header";
 import "./Details.css";
 import YouTube from "react-youtube";
-import RatingStars from "../../commom/RatingStars";
+import RatingStars from "../../commom/ratingStars/RatingStars";
 
 // material-ui imports
 import Typography from "@material-ui/core/Typography";
@@ -26,17 +26,105 @@ function Details(props) {
       });
   };
 
+  useEffect(() => {
+    getMovieDetails();
+  }, []);
+
+  function _onReady(event) {
+    event.target.pauseVideo();
+  }
+  const opts = {
+    width: "100%",
+  };
 
   return (
     <div className="details-page-container">
-      <Header
-        baseUrl={props.baseUrl}
+      <Header baseUrl={props.baseUrl}
         movieId={props.match.params.id}
-        history={props.history}
-      />
-      
+        history={props.history}/>
+      <Typography
+        component="div"
+        className="back-to-home-btn"
+        onClick={() => {
+          props.history.push("/");
+        }}
+      >
+        &#60; Back to Home
+      </Typography>
+      {movieDetails && (
+        <div className="detail-container">
+          <div className="image-poster">
+            <img src={movieDetails.poster_url} alt={movieDetails.title} />
+          </div>
+
+          <div className="movie-details">
+            <Typography component="h2" variant="headline">
+              {movieDetails.title}
+            </Typography>
+            <Typography component="div">
+              <span className="bold">Genre:</span>{" "}
+              {movieDetails.genres.join(",")}
+            </Typography>
+            <Typography component="div">
+              <span className="bold">Duration:</span> {movieDetails.duration}
+            </Typography>
+            <Typography component="div">
+              <span className="bold">Release Date:</span>{" "}
+              {movieDetails.release_date}
+            </Typography>
+            <Typography component="div">
+              <span className="bold">Rating:</span> {movieDetails.rating}
+            </Typography>
+            <br />
+
+            <Typography component="div">
+              <span className="bold">Plot:</span>{" "}
+              <a href={movieDetails.wiki_url}>(Wiki Link)</a>
+              {movieDetails.storyline}
+            </Typography>
+            <br />
+
+            <Typography className="bold" component="div">
+              Trailer:
+            </Typography>
+            <YouTube
+              className="youtube-video"
+              videoId={new URL(movieDetails.trailer_url).searchParams.get("v")}
+              opts={opts}
+              onReady={_onReady}
+            />
+          </div>
+          <div className="artist-img">
+            <Typography className="bold" component="div">
+              Rate this movie:
+            </Typography>
+            <RatingStars />
+
+            <Typography className="bold artists_text" component="div">
+              Artists:
+            </Typography>
+
+            {movieDetails.artists && (
+              <Fragment>
+                <GridList className="movie-artists" cols={2} spacing={16}>
+                  {movieDetails.artists.map((artist) => (
+                    <GridListTile key={artist.profile_url}>
+                      <img
+                        src={artist.profile_url}
+                        alt={`${artist.first_name} ${artist.last_name}`}
+                      />
+                      <GridListTileBar
+                        title={`${artist.first_name} ${artist.last_name}`}
+                      />
+                    </GridListTile>
+                  ))}
+                </GridList>
+              </Fragment>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
-
 export default Details;
