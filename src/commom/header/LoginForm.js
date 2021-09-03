6df84a4eed86b26f368eb287ef67ffb6f221b272
Prajwalujjani <prajwalbunty22@gmail.com>
2021-09-03 22:40:20 +0530

    Form validation is implemented

--- src/commom/header/LoginForm.js
+++ src/commom/header/LoginForm.js
@@ -1,50 +1,95 @@
-import React, {useState} from 'react'
-import { TextField, Button } from '@material-ui/core'
-import './LoginResgisterForm.css'
-
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import "./LoginResgisterForm.css";
 
 function LoginForm(props) {
-const [email, setEmail] = useState("")
-const [Password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function Login(){
-     
-    const authorization=window.btoa(email+":"+Password)
-    const headers= {
-        "Accept": "application/json;charset=UTF-8",
-        "authorization":"Basic " + authorization
-      }        
-   
+  const [validationError, setvalidationError] = useState({
+    email: false,
+    password: false,
+  });
+
+  function Login() {
+    const validationErrorValues = { ...validationError }; //e:false, p:false
+
+    if (email === "") {
+      validationErrorValues.email = true; //e:true, p:false
+    }
+    if (password === "") {
+      validationErrorValues.password = true; //e:true, p:true
+    }
+    if (validationErrorValues.email || validationErrorValues.password) {
+      setvalidationError(validationErrorValues);
+      return;
+    }
+
+    const authorization = window.btoa(email + ":" + password);
+    const headers = {
+      Accept: "application/json;charset=UTF-8",
+      authorization: "Basic " + authorization,
+    };
+
     fetch(props.baseUrl + "auth/login", {
-        method: "POST",
-        headers        
+      method: "POST",
+      headers,
     })
-        .then((response) =>  
-        response.json())
-        .then((data) => {
-          //setBookingId(data.reference_number);
-          const userDetails = {data, loginStatus:true}
-            localStorage.setItem("userDetails", JSON.stringify(userDetails))
-            props.setLoginStatus(true)
-            props.setIsOpen(false)
-        });      
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "ACTIVE") {
+          const userDetails = { data, loginStatus: true };
+          localStorage.setItem("userDetails", JSON.stringify(userDetails));
+          props.setLoginStatus(true);
+          props.setIsOpen(false);
+        }
+      });
+  }
+
+  function resetValidationError(fieldName) {
+    const validationErrorValues = { ...validationError };
+    validationErrorValues[fieldName] = false;
+    setvalidationError(validationErrorValues);
+  }
+
+  return (
+    <div className="center">
+      <form>
+        <TextField
+          required
+          label="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+            resetValidationError("email");
+          }}
+          error={validationError.email}
+          helperText={validationError.email ? "Required" : ""}
+          value={email}
+        />
+        <br />
+
+        <TextField
+          type="password"
+          label="Password"
+          required
+          onChange={(e) => {
+            setPassword(e.target.value);
+            resetValidationError("password");
+          }}
+          error={validationError.password}
+          helperText={validationError.password ? "Required" : ""}
+          value={password}
+        />
+        <br />
+        <br />
+        <br />
+      </form>
+
+      <Button onClick={Login} variant="contained" color="primary">
+        Login
+      </Button>
+    </div>
+  );
 }
 
-    return (
-        <div className="center">
-            <form >
-                <TextField label="Username" required 
-                onChange={(e)=>setEmail(e.target.value)}
-                value={email}/><br />
-                <TextField label="Password" required 
-                onChange={(e)=>setPassword(e.target.value)}
-                value={Password}/><br /><br /><br />                               
-            </form>
-            <Button onClick={Login} variant="contained" color="primary" >
-                    Login
-            </Button> 
-        </div> 
-    )
-}
-
-export default LoginForm
+export default LoginForm;
--- src/commom/header/RegisterForm.js
+++ src/commom/header/RegisterForm.js
@@ -1,70 +1,158 @@
-import React, { useState } from 'react'
-import { TextField, Button } from '@material-ui/core'
-import './LoginResgisterForm.css'
-
-
-
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import "./LoginResgisterForm.css";
 
 function RegisterForm(props) {
-    const [firstname, setFirstname] = useState("")
-    const [lastname, setLastname] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [contactno, setContactno] = useState("")
-    
-    function Register(){
-        console.warn(firstname,lastname,email,password,contactno)  
-        const data= {
-            "email_address": email,
-            "first_name": firstname,
-            "last_name": lastname,
-            "mobile_number": contactno,
-            "password": password
-          }        
-       
-        fetch(props.baseUrl + "signup", {
-            method: "POST",
-            headers: {
-                "Accept": "application/json;charset=UTF-8",
-                "Content-Type" : "application/json;charset=UTF-8"
-            },
-            body: JSON.stringify(data),
-        }).then((response) =>  
-            response.json())
-            .then((data) => {
-              //setBookingId(data.reference_number);
-              const userDetails = {data, loginStatus:true}
-                localStorage.setItem("userDetails", JSON.stringify(userDetails))
-                props.setLoginStatus(true)
-                props.setIsOpen(false)
-            });      
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [contactno, setContactno] = useState("");
+
+  const [validationError, setvalidationError] = useState({
+    firstname: false,
+    lastname: false,
+    email: false,
+    password: false,
+    contactno: false,
+  });
+
+  function Register() {
+    const validationErrorValues = { ...validationError };
+
+    if (firstname === "") {
+      validationErrorValues.firstname = true;
     }
-    
-   
-    return (
-        <div className="center">
-            <form >
-                <TextField label="First Name"
-                onChange={(e)=>setFirstname(e.target.value)} value={firstname}
-                    required /><br />
-                <TextField label="Last Name"
-                onChange={(e)=>setLastname(e.target.value)} value={lastname}
-                    required /><br />
-                <TextField label="Email"
-                onChange={(e)=>setEmail(e.target.value)} value={email}
-                    required /><br />
-                <TextField label="Password" type="password"
-                onChange={(e)=>setPassword(e.target.value)} value={password}
-                    required /><br />
-                <TextField label="Contact No"
-                onChange={(e)=>setContactno(e.target.value)} value={contactno}
-                    required /><br /><br /><br /><br /> 
-            </form>
-            <Button onClick={Register} variant="contained" color="primary" >
-                Register
-            </Button>
-        </div>
-    )
+    if (lastname === "") {
+      validationErrorValues.lastname = true;
+    }
+    if (email === "") {
+      validationErrorValues.email = true;
+    }
+    if (password === "") {
+      validationErrorValues.password = true;
+    }
+    if (contactno === "") {
+      validationErrorValues.contactno = true;
+    }
+
+    if (
+      validationErrorValues.firstname ||
+      validationErrorValues.lastname ||
+      validationErrorValues.email ||
+      validationErrorValues.password ||
+      validationErrorValues.contactno
+    ) {
+      setvalidationError(validationErrorValues);
+      return;
+    }
+
+    console.warn(firstname, lastname, email, password, contactno);
+    const data = {
+      email_address: email,
+      first_name: firstname,
+      last_name: lastname,
+      mobile_number: contactno,
+      password: password,
+    };
+
+    fetch(props.baseUrl + "signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json;charset=UTF-8",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        //setBookingId(data.reference_number);
+        const userDetails = { data, loginStatus: true };
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+        props.setLoginStatus(true);
+        props.setIsOpen(false);
+      });
+  }
+
+  function resetValidationError(fieldName) {
+    const validationErrorValues = { ...validationError };
+    validationErrorValues[fieldName] = false;
+    setvalidationError(validationErrorValues);
+  }
+
+  return (
+    <div className="center">
+      <form>
+        <TextField
+          label="First Name"
+          onChange={(e) => {
+            setFirstname(e.target.value);
+            resetValidationError("firstname");
+          }}
+          error={validationError.firstname}
+          helperText={validationError.firstname ? "Required" : ""}
+          value={firstname}
+          required
+        />
+        <br />
+        <TextField
+          label="Last Name"
+          onChange={(e) => {
+            setLastname(e.target.value);
+            resetValidationError("lastname");
+          }}
+          error={validationError.lastname}
+          helperText={validationError.lastname ? "Required" : ""}
+          value={lastname}
+          required
+        />
+        <br />
+        <TextField
+          label="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+            resetValidationError("email");
+          }}
+          error={validationError.email}
+          helperText={validationError.email ? "Required" : ""}
+          value={email}
+          required
+        />
+        <br />
+        <TextField
+          label="Password"
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+            resetValidationError("password");
+          }}
+          value={password}
+          error={validationError.password}
+          helperText={validationError.password ? "Required" : ""}
+          required
+        />
+        <br />
+        <TextField
+          label="Contact No"
+          onChange={(e) => {
+            setContactno(e.target.value);
+            resetValidationError("contactno");
+          }}
+          error={validationError.contactno}
+          helperText={validationError.contactno ? "Required" : ""}
+          value={contactno}
+          required
+        />
+        <br />
+        <br />
+        <br />
+        <br />
+      </form>
+      <Button onClick={Register} variant="contained" color="primary">
+        Register
+      </Button>
+    </div>
+  );
 }
 
-export default RegisterForm
+export default RegisterForm;
